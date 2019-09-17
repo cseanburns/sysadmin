890b007a06332b061f930697576fc0a7948df5df
C. Sean Burns <sean.csb@gmail.com>
2019-09-17 08:27:38 -0400

    completed previous edit

--- usr/facultycount.sh
+++ usr/facultycount.sh
@@ -7,7 +7,7 @@
 
 totalfac=$(grep "faculty" /etc/group | sed -e 's/:/\n/g' | tail -n1 | sed -e 's/,/\n/g' | wc -l)
 printf "\n"
-printf "There are $(($totalfac)) faculty or staff accounts.\n"
+printf "There are $totalfac faculty or staff accounts.\n"
 printf "\n"
 printf "They include the following:\n"
 grep "faculty" /etc/group | sed -e 's/:/\n/g' | tail -n1 | sed -e 's/,/\n/g'
--- usr/studentcount.sh
+++ usr/studentcount.sh
@@ -6,7 +6,7 @@
 # list them
 
 totalstudents=$(grep "students" /etc/group | sed -e 's/:/\n/g' | tail -n1 | sed -s 's/,/\n/g' | wc -l)
-printf "\nThere are $(($totalstudents)) student accounts.\n"
+printf "\nThere are $totalstudents student accounts.\n"
 printf "\nThey include the following:\n"
 printf "\n"
 grep "students" /etc/group | sed -e 's/:/\n/g' | tail -n1 | sed -e 's/,/\n/g' | pr -T -3
