4b4aab2c90ea3dfb6f0e04dd859053ec6de68b30
cseanburns <sean.csb@gmail.com>
2020-04-27 17:18:45 -0400

    reduced to function; made output prettier

--- games/anagram-solver.py
+++ games/anagram-solver.py
@@ -1,54 +1,47 @@
 #!/usr/bin/python3 
 
-# Created:  Date: Thu Jan  2 00:12:57 EST 2020
-# Revised: Date: Mon Apr 27 00:31:27 EDT 2020
+# Started on:   Date: Thu Jan  2 00:12:57 EST 2020
+# Revisited on: Date: Mon Apr 27 00:31:27 EDT 2020
+# Revised:      Date: Mon Apr 27 17:12:12 EDT 2020
 #
 # To Do:
 # 1. print only those matches that have the essential letter in the
 # anagram. For example, the NY Times Spelling Bee game requires one
 # specific letter in all found words
-# 2. Make pretty
- 
+# 2. check data input strings are strings, integers are integers
+
+# sys is for inputing integers and product is for creating letter
+# combinations
+import sys
 from itertools import product
 
-# take letters for anagram
-letters = input()
+# Take as input some scrambled letters and then ask for length of word
+# formations. On my laptop, going past 8 is too computationally
+# demanding, so warn against that.
+#
+# Compare to a wordlist: based on the american-english dictionary
+# located at /usr/share/dict/american-english.
+#
+# The dictionary file has been cleaned up in Bash (removed punctuations,
+# lower cased, etc) and the file was renamed as 'wordlist'
 
-# get cominations based on various lengths from 4 - 8 letters long
-product0 = product(letters, repeat = 4)
-result0 = []
-for i in product0:
-    result0.append(''.join(i))
+def scramble():
+    print("Enter letters: ")
+    letters = input()
+    print("Enter number of repeats (no more than 8): ")
+    repeats = input()
+    repeats = int(repeats)
+    suggestions = product(letters, repeat = repeats)
+    result = []
+    for i in suggestions:
+        result.append(''.join(i))
 
-product1 = product(letters, repeat = 5)
-result1 = []
-for i in product1:
-    result1.append(''.join(i))
+    with open('wordlist', 'r') as wordlist:
+        comparewords = wordlist.read().splitlines()
+            
+    print(*set(result).intersection(comparewords))
 
-product2 = product(letters, repeat = 6)
-result2 = []
-for i in product2:
-    result2.append(''.join(i))
+def main():
+    scramble()
 
-product3 = product(letters, repeat = 7)
-result3 = []
-for i in product3:
-    result3.append(''.join(i))
-
-product4 = product(letters, repeat = 8)
-result4 = []
-for i in product4:
-    result4.append(''.join(i))
-
-# read in the word list from the american-english dictionary:
-# /usr/share/dict/american-english. The file has already been cleaned up
-# in Bash (punctuation removed, converted to lowercase, etc)
-with open('wordlist', 'r') as wordlist:
-    comparewords = wordlist.read().splitlines()
-
-# print out the combinations of word
-print(set(result0).intersection(comparewords))
-print(set(result1).intersection(comparewords))
-print(set(result2).intersection(comparewords))
-print(set(result3).intersection(comparewords))
-print(set(result4).intersection(comparewords))
+main()
