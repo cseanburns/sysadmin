820b1e2351ceb6f9bf0a3cd42a8a5996d41a1994
cseanburns <sean.csb@gmail.com>
2020-04-28 15:26:34 -0400

    office hour script physical signage

---
+++ games/officehours.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os 
+from datetime import datetime
+
+officeday = datetime.today().strftime('%a')
+day1 = "Tue"
+day2 = "Wed"
+email = "myemail@uky.edu"
+
+os.system("clear")
+print("Christopher Sean Burns, PhD")
+print("Associate Professor")
+print(email)
+print("859-555-0000")
+print("Spring 2020")
+
+if officeday == day1:
+    print("\nSince today is {}, my office hours are 1-3pm".format(day1))
+elif officeday == day2:
+    print("\nSince today is {}, my office hours are 1-3pm".format(day2))
+else:
+    print("\nSince today is neither {} nor {},".format(day1,day2))
+    print("please knock on door or email me at {}".format(email))
+    print("to schedule an appointment.")
