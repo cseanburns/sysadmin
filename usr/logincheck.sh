ebad50f8ac8e65e40aa56b41e1e1b1e5670a27aa
C. Sean Burns <sean.csb@gmail.com>
2020-08-24 15:18:37 -0400

    new script logging user activity

---
+++ usr/logincheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Date: 2020-08-24
+
+# check to see if users are logging in and running commands
+# run as root and put in crontab
+# file 'accountsnames' includes user accounts, one line each
+
+while read i ; do
+	echo "$i:" ; tail -n2 "/home/$i/.bash_history" ; echo -e "\n"
+done < /root/bin/accountsnames > /root/logs/$(date +%Y-%m-%d).txt
+
+# Convert dates
+/root/bin/dateconvert.sh  /root/logs/$(date +%Y-%m-%d).txt
