5409a96c7d4962671c2035c86bebf2bee692e8e7
C. Sean Burns <sean.csb@gmail.com>
2020-10-20 22:30:26 -0400

    adjusted printf statement

--- usr/studentcount.sh
+++ usr/studentcount.sh
@@ -5,8 +5,8 @@
 # Get a count of all members of the 'students' group on the server and
 # list them
 
-totalstudents=$(grep "students" /etc/group | sed -e 's/:/\n/g' | tail -n1 | sed -s 's/,/\n/g' | wc -l)
-printf "\nThere are $totalstudents student accounts.\n"
+total_students=$(grep "students" /etc/group | sed -e 's/:/\n/g' | tail -n1 | sed -s 's/,/\n/g' | wc -l)
+printf "\nThere are %s student accounts.\n" "$total_students"
 printf "\nThey include the following:\n"
 printf "\n"
 grep "students" /etc/group | sed -e 's/:/\n/g' | tail -n1 | sed -e 's/,/\n/g' | pr -T -3
