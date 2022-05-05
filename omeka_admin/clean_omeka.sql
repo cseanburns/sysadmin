72d96f844fab1efedcccf71756906d1d27cc4425
C. Sean Burns <sean.csb@gmail.com>
2022-05-05 12:43:02 -0400

    added comments

--- omeka_admin/clean_omeka.sql
+++ omeka_admin/clean_omeka.sql
@@ -1,3 +1,18 @@
+/*
+Date: Thu May  5 12:42:04 PM EDT 2022
+
+Purpose:
+This script removes all past users and content from an Omeka installation.
+This script assumes that the Omeka database is named "lis602_omeka".
+Feel free to change if the database has a different name by changing
+the name in the first line of code below.
+
+To run:
+Login to MySQL
+Select Omeka Database
+Run script at MySQL prompt: source script.sql
+*/
+
 use lis602_omeka;
 delete from omeka_collections where owner_id > 0;
 delete from omeka_files where id > 0;
